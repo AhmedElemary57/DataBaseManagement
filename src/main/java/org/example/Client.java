07add7a6f88f2522deb550aea119a3166fdfbd69
elemary <honor5766@gmail.com>
2022-08-30 20:48:04 +0200

    Client GUI

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -7,8 +7,52 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
+
     static ServerSocket clientSocket;
 
+     String setRequest(String key, String value) throws IOException {
+         int numberOfNodes=5;
+         Random rand = new Random();
+         int randomConnectToNode = rand.nextInt(numberOfNodes)+1;
+         Socket socket = new Socket("localhost", 5000 + randomConnectToNode);
+         OutputStream outputStream = socket.getOutputStream();
+         // create a data output stream from the output stream so we can send data through it
+         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+         String input = "set(" + key + "," + value + ")";
+         dataOutputStream.writeUTF(input);
+         dataOutputStream.flush(); // send the message
+
+         InputStream inputStream = socket.getInputStream();
+         // create a DataInputStream so we can read data from it.
+         DataInputStream dataInputStream = new DataInputStream(inputStream);
+         // read the message from the socket
+         String message = dataInputStream.readUTF();
+         System.out.println("Received from server: " +message);
+         return message;
+     }
+     String getRequest(String key) throws IOException {
+            int numberOfNodes=5;
+            Random rand = new Random();
+            int randomConnectToNode = rand.nextInt(numberOfNodes)+1;
+            Socket socket = new Socket("localhost", 5000 + randomConnectToNode);
+            OutputStream outputStream = socket.getOutputStream();
+            // create a data output stream from the output stream so we can send data through it
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            String input = "get(" + key + ")";
+
+            dataOutputStream.writeUTF(input);
+
+            dataOutputStream.flush(); // send the message
+            InputStream inputStream = socket.getInputStream();
+
+            // create a DataInputStream so we can read data from it.
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            // read the message from the socket
+            String message = dataInputStream.readUTF();
+            System.out.println("Received from server: " +message);
+            return message;
+
+     }
 
     public static void main(String[] args) throws IOException {
 
---
+++ src/main/java/org/example/ClientGUI.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ClientGUI extends JFrame {
+    private JPanel mainPanel;
+    private JTextField setKet;
+    private JTextField setValue;
+    private JButton setButton;
+    private JTextField getValue;
+    private JButton getButton;
+    private JTextArea out;
+    private JLabel yourOutputLabel;
+    Client client;
+
+
+    public ClientGUI()  {
+        super("Client");
+        client = new Client();
+
+        this.setButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (setKet.getText().isEmpty() || setValue.getText().isEmpty()) {
+                    out.setText("Please enter a key and a value");
+                } else {
+                    try {
+                        String response=client.setRequest(setKet.getText(), setValue.getText());
+                        out.append(response+"\n");
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        this.getButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (getValue.getText().isEmpty()) {
+                    out.setText("Please enter a key");
+                } else {
+                    try {
+                        String response=client.getRequest(getValue.getText());
+                        out.append("the key "+getValue.getText()+" has value of :"+response+"\n");
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    private void createUIComponents() {
+
+
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new ClientGUI();
+        frame.setPreferredSize(new Dimension(600, 600));
+        frame.setContentPane(new ClientGUI().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
