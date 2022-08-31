14838edac8d003d3020a161ca2c413273f5b9ce1
al-sharif-mohamed <el-sharif-mohamed-22@outlook.com>
2022-08-31 09:22:42 +0200

    Fixed shell script Fixed Admin.java

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -4,15 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 
 public class Admin {
 
     public static void main(String[] args) throws IOException {
 
-        ProcessBuilder builder = new ProcessBuilder("/home/elemary/Projects/DataBaseManagement/src/main/java/org/example/test.sh");
-        builder.directory(new File(System.getProperty("user.home")));
-        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
-        builder.start();
+        ProcessBuilder pb = new ProcessBuilder("IdeaProjects/DataBaseManagement/src/main/java/org/example/test.sh");
+        pb.directory(new File(System.getProperty("user.home")));
+        pb.start();
     }
 }
--- src/main/java/org/example/test.sh
+++ src/main/java/org/example/test.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 for i in {1..5}
 do
-     gnome-terminal -e "sh -c \"java -jar /home/elemary/Projects/DataBaseManagement/out/artifacts/DataBaseManagement_jar/DataBaseManagement.jar $i 5 20 4 10; sleep 10; exec bash\""
-    # java -jar /home/ahmed/Desktop/Project/Server/Server/out/artifacts/Server_jar/Server.jar $i 5 
+   gnome-terminal -- bash -c "java -jar /home/al-sharif-mohamed/IdeaProjects/DataBaseManagement/out/artifacts/DataBaseManagement_jar/DataBaseManagement.jar $i 5 20 3 10 5 2 4; exec bash"
 done
