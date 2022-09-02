4f51be80f112b179f23d67b378de3aa9bea5a065
Ahmed elemary <60315006+ahmedelemary57@users.noreply.github.com>
2022-09-02 10:36:31 +0200

    Admin have variable paths

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -7,15 +7,22 @@ import java.util.List;
 
 
 public class Admin {
+    private static String[] mohamed = new String[]{"/home/al-sharif-mohamed/IdeaProjects/DataBaseManagement/src/main/java/org/example/test.sh",
+            "/home/al-sharif-mohamed/IdeaProjects/DataBaseManagement/out/artifacts/DataBaseManagement_jar/DataBaseManagement.jar",
+            "IdeaProjects/DataBaseManagement/src/main/java/org/example/test.sh"};
+    private static String[] elemary = new String[] {"/home/elemary/Projects/DataBaseManagement/src/main/java/org/example/test.sh",
+            "/home/elemary/Projects/DataBaseManagement/out/artifacts/DataBaseManagement_jar/DataBaseManagement.jar",
+            "Projects/DataBaseManagement/src/main/java/org/example/test.sh"
+    };
     public static int count = 0;
     public static void startServers(List<Integer> data) throws IOException {
         // write data into test.sh file
         try {
-            FileWriter myWriter = new FileWriter("/home/elemary/Projects/DataBaseManagement/src/main/java/org/example/test.sh");
+            FileWriter myWriter = new FileWriter(mohamed[0]);
             myWriter.write("#!/bin/bash\n" +
                     "for i in {1.." + data.get(0) + "}\n" +
                     "do\n" +
-                    "   gnome-terminal -- bash -c \"java -jar /home/elemary/Projects/DataBaseManagement/out/artifacts/DataBaseManagement_jar/DataBaseManagement.jar $i "
+                    "   gnome-terminal -- bash -c \"java -jar " + mohamed[1] + " $i "
                     + data.get(0) + " " + data.get(1) + " " + data.get(2) + " " + data.get(3) +
                     " " + data.get(4) + " " + data.get(5) + " " + data.get(6) + "; exec bash\"\n" +
                     "done\n");
@@ -25,16 +32,16 @@ public class Admin {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        ProcessBuilder pb = new ProcessBuilder("Projects/DataBaseManagement/src/main/java/org/example/test.sh");
+        ProcessBuilder pb = new ProcessBuilder(mohamed[2]);
         pb.directory(new File(System.getProperty("user.home")));
         pb.start();
     }
     public static void addNode(List<Integer> data) throws IOException {
         // write data into test.sh file
         try {
-            FileWriter myWriter = new FileWriter("/home/elemary/Projects/DataBaseManagement/src/main/java/org/example/test.sh");
+            FileWriter myWriter = new FileWriter(mohamed[0]);
             myWriter.write("#!/bin/bash\n" +
-                    "gnome-terminal -- bash -c \"java -jar /home/elemary/Projects/DataBaseManagement/out/artifacts/DataBaseManagement_jar/DataBaseManagement.jar "
+                    "gnome-terminal -- bash -c \"java -jar " + mohamed[1] + " "
                     + count + " "
                     + count + " " + data.get(1) + " " + data.get(2) + " " + data.get(3) +
                     " " + data.get(4) + " " + data.get(5) + " " + data.get(6) + "; exec bash\"\n" );
@@ -49,7 +56,7 @@ public class Admin {
             e.printStackTrace();
         }
         System.out.println(count);
-        ProcessBuilder pb = new ProcessBuilder("Projects/DataBaseManagement/src/main/java/org/example/test.sh");
+        ProcessBuilder pb = new ProcessBuilder(mohamed[2]);
         pb.directory(new File(System.getProperty("user.home")));
         pb.start();
     }
